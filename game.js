5eb5c87a8b6846986d88b137b9c0d51779444dbf
mushi1024 <gary801024@gmail.com>
2025-07-14 10:44:04 +0800

    Update game.js

--- game.js
+++ game.js
@@ -153,3 +153,33 @@ function startGame() {
 }
 
 startGame();
+
+
+let touchStartX = 0;
+let touchStartY = 0;
+
+document.addEventListener("touchstart", function (e) {
+  if (e.touches.length !== 1) return;
+  touchStartX = e.touches[0].clientX;
+  touchStartY = e.touches[0].clientY;
+});
+
+document.addEventListener("touchend", function (e) {
+  if (e.changedTouches.length !== 1) return;
+  const dx = e.changedTouches[0].clientX - touchStartX;
+  const dy = e.changedTouches[0].clientY - touchStartY;
+
+  if (Math.abs(dx) > Math.abs(dy)) {
+    if (dx > 30) {
+      moveRight();
+    } else if (dx < -30) {
+      moveLeft();
+    }
+  } else {
+    if (dy > 30) {
+      moveDown();
+    } else if (dy < -30) {
+      moveUp();
+    }
+  }
+});
